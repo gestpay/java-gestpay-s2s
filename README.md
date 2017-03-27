8fed655ace6ad5be7787fc049d9a2814165b919c
musikele <michele.nasti@gmail.com>
2017-03-27 17:56:31 +0200

    GD-188 callRequestTokenS2SExample

--- README.md
+++ README.md
@@ -18,3 +18,20 @@ In case of changes to the endpoint, you might want to regenerate classes. You ca
   https://testecomm.sella.it/gestpay/gestpayws/WSs2s.asmx?WSDL
   ```
 
+## Debugging
+To see the actual `http` and `xml` data passing over the net, you can add this snippet at the top of the class to test:
+
+ ```java
+
+public class ClassToTest {
+
+	static {
+		System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
+		System.setProperty("com.sun.xml.ws.transport.http.HttpAdapter.dump", "true");
+		System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
+		System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dump", "true");
+	}
+	...
+}
+ ```
+
---
+++ src/main/java/examples/CallRequestTokenS2SExample.java
@@ -0,0 +1,60 @@
+package examples;
+
+import it.gestpay.wss2s.model.CallRequestTokenS2SResponse;
+import it.gestpay.wss2s.model.WSs2S;
+import it.gestpay.wss2s.model.WSs2SSoap;
+import org.w3c.dom.Element;
+
+import java.util.logging.Logger;
+
+import static util.Utils.logNode;
+import static util.Utils.showErrorMessage;
+
+/**
+ * This is an example of calling callRequestTokenS2S of WsS2S.
+ *
+ * More info here: http://docs.gestpay.it/adv/request-tokens.html
+ *
+ * Api details: http://api.gestpay.it/#callrequesttokens2s
+ *
+ */
+public class CallRequestTokenS2SExample {
+
+  private static Logger logger = Logger.getLogger("CallRequestTokenS2SExample");
+
+  public static void main(String... args) {
+
+    //writes logs on a single line
+    System.setProperty("java.util.logging.SimpleFormatter.format",
+        "%1$tF %1$tT %4$s %2$s %5$s%6$s%n");
+
+    //instantiate the webservice
+    WSs2S wSs2S = new WSs2S();
+    WSs2SSoap wSs2SSoap = wSs2S.getWSs2SSoap();
+
+    //input data
+    String shopLogin = "GESPAY65987";
+    String requestToken = "MASKEDPAN";
+    String withAuth = "Y";
+
+    // credit card details
+    String cardNumber = "5412388800001018"; //test credit card
+    String expMonth = "05";
+    String expYear = "17";
+    String cvv = "999";
+
+    //call Gestpay ...
+    CallRequestTokenS2SResponse.CallRequestTokenS2SResult callRequestTokenS2SResult = wSs2SSoap.callRequestTokenS2S(shopLogin,
+        requestToken, cardNumber, expMonth, expYear, cvv, withAuth);
+
+    if (callRequestTokenS2SResult.getContent() == null) {
+      showErrorMessage();
+    }
+
+    // use DOM api to retrieve data from webservice
+    Element response = (Element) callRequestTokenS2SResult.getContent().get(0);
+
+    logNode(response);
+
+  }
+}
